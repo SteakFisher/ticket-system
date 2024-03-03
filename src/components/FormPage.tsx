43143de9e6601671ea0472ed1f9147f032bb356c
SteakFisher <miniminter2018p@gmail.com>
2024-03-04 03:59:17 +0530

    No reads from db

--- src/components/FormPage.tsx
+++ src/components/FormPage.tsx
@@ -48,10 +48,8 @@ export default function FormElement() {
         console.log(e)
         const { data: { user }} = await supabase.auth.getUser()
         console.log(user)
-        const {data} = await supabase.from("Guests").insert({alias: e.alias, altEmail: e.altEmail, isVeg: e.isVeg as unknown as boolean, email: user?.email}).select("*")
-        if (data) {
-          router.push("/form/success")
-        }
+        await supabase.from("Guests").insert({alias: e.alias, altEmail: e.altEmail, isVeg: e.isVeg as unknown as boolean, email: user?.email})
+        router.push("/form/success")
 
       })} className="space-y-8">
         <FormField
